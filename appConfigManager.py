e815c1c16499e1a941c821ce8f98e7f2da5c5969
Ahmed Aleryani <ahmed.m.aleryani@gmail.com>
2021-02-13 01:08:31 +0300

    Feat: add upload to s3 storage service using python.
    I have added the code which can be used to create a bucket using s3 in Aws and the upload file to the created bucket.

--- appConfigManager.py
+++ appConfigManager.py
@@ -1,20 +1,23 @@
+import logging
+
 import boto3
-def initilizeBoto(clientName):
-    boto3.client(clientName)
+from botocore.exceptions import ClientError
+region="us-east-2"
+client = boto3.client("appconfig")
+s3 = boto3.resource('s3') #used with put object and Bucket and also get all buckets
+# s3 = boto3.client('s3',region_name=region)
 
 def createApplication():
-    client = initilizeBoto("example")
     response = client.create_application(
-        Name='string',
-        Description='string',
-        Tags={
-            'string': 'string'
-        }
+        Name='Test from python 2',
+        Description='this is nothing',
+        # Tags={
+        #     'string': 'string'
+        # }
     )
     return response
 
 def createConfigFile():
-    client = initilizeBoto("example")
     response = client.create_configuration_profile(
         ApplicationId='someId will be here',
         Name='string',
@@ -32,3 +35,20 @@ def createConfigFile():
         }
     )
     return response
+# print(client)
+# for bucket in s3.buckets.all():
+#     print(bucket.name)
+
+
+
+#I created bucket on the aws using the following code inisde try statements
+# try:
+#     location = {'LocationConstraint':region}
+#     s3.create_bucket(Bucket="ahmed-bucket-app-config",CreateBucketConfiguration=location)
+# except ClientError as e:
+#     logging.error(e)
+
+data=open("appConfigManager.py","rb")
+s3.Bucket("ahmed-bucket-app-config").put_object(Key="appConfigManager.py",Body=data)# I uploaded the file I am using now using this line of code
+
+# print(createApplication())
